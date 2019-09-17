9ee53ef225f656699942b263c3504a56dd675107
Phil Fitch <mostreluctantly@gmail.com>
2019-09-17 08:43:10 -0700

    basic validations pass

---
+++ __tests__/validator.test.js
@@ -0,0 +1,140 @@
+const validator = require('../lib/validator.js');
+
+describe('validator module', () => {
+  
+  const str = 'yes';
+  const num = 1;
+  const arr = ['a'];
+  const obj = { x: 'y' };
+  const func = () => {};
+  const bool = false;
+
+  describe('performs basic validation of', () => {
+
+    it('strings', () => {
+      expect(validator.isString(str)).toBeTruthy();
+      expect(validator.isString(num)).toBeFalsy();
+      expect(validator.isString(arr)).toBeFalsy();
+      expect(validator.isString(obj)).toBeFalsy();
+      expect(validator.isString(func)).toBeFalsy();
+      expect(validator.isString(bool)).toBeFalsy();
+    });
+
+    it('numbers', () => {
+      expect(validator.isNum(str)).toBeFalsy();
+      expect(validator.isNum(num)).toBeTruthy();
+      expect(validator.isNum(arr)).toBeFalsy();
+      expect(validator.isNum(obj)).toBeFalsy();
+      expect(validator.isNum(func)).toBeFalsy();
+      expect(validator.isNum(bool)).toBeFalsy();
+    });
+
+    it('arrays', () => {
+      expect(validator.isArray(str)).toBeFalsy();
+      expect(validator.isArray(num)).toBeFalsy();
+      expect(validator.isArray(arr)).toBeTruthy();
+      expect(validator.isArray(obj)).toBeFalsy();
+      expect(validator.isArray(func)).toBeFalsy();
+      expect(validator.isArray(bool)).toBeFalsy();
+    });
+
+    it('objects', () => {
+      expect(validator.isObject(str)).toBeFalsy();
+      expect(validator.isObject(num)).toBeFalsy();
+      expect(validator.isObject(arr)).toBeFalsy();
+      expect(validator.isObject(obj)).toBeTruthy();
+      expect(validator.isObject(func)).toBeFalsy();
+      expect(validator.isObject(bool)).toBeFalsy();
+    });
+
+    it('booleans', () => {
+      expect(validator.isBoolean(str)).toBeFalsy();
+      expect(validator.isBoolean(num)).toBeFalsy();
+      expect(validator.isBoolean(arr)).toBeFalsy();
+      expect(validator.isBoolean(obj)).toBeFalsy();
+      expect(validator.isBoolean(func)).toBeFalsy();
+      expect(validator.isBoolean(bool)).toBeTruthy();
+    });
+
+    it('functions', () => {
+      expect(validator.isFunction(str)).toBeFalsy();
+      expect(validator.isFunction(num)).toBeFalsy();
+      expect(validator.isFunction(arr)).toBeFalsy();
+      expect(validator.isFunction(obj)).toBeFalsy();
+      expect(validator.isFunction(func)).toBeTruthy();
+      expect(validator.isFunction(bool)).toBeFalsy();
+    });
+  });
+
+  describe('performs array validation of', () => {
+
+    const arrayOfStrings = ['a', 'b', 'c'];
+    const arrayOfNumbers = [1, 2, 3];
+    const arrayOfObjects = [{}, {}, {}];
+    const arrayOfBooleans = [true, false, true];
+
+    it('strings', () => {
+      expect(validator.isArrayOfStrings(arrayOfStrings)).toBeTruthy();
+      expect(validator.isArrayOfStrings(arrayOfNumbers)).toBeFalsy();
+      expect(validator.isArrayOfStrings(arrayOfObjects)).toBeFalsy();
+      expect(validator.isArrayOfStrings(arrayOfBooleans)).toBeFalsy();
+    });
+
+    it.skip('numbers', () => {
+
+    });
+
+    it.skip('objects', () => {
+    
+    });
+
+    it.skip('booleans', () => {
+    
+    });
+  });
+
+  describe('get validator for', () => {
+
+    it('strings', () => {
+      // TODO: pass getValidator the rules
+      expect(validator.getValidator(/* rules */)).toBe(validator.isString);
+    });
+    
+    it.skip('numbers', () => {
+      
+    });
+
+    it.skip('arrays', () => {
+      
+    });
+
+    it.skip('objects', () => {
+      
+    });
+
+    it.skip('booleans', () => {
+      
+    });
+
+    it.skip('functions', () => {
+      
+    });
+
+    it.skip('array of strings', () => {
+      
+    });
+
+    it.skip('array of numbers', () => {
+      
+    });
+
+    it.skip('array of objects', () => {
+      
+    });
+
+    it.skip('array of booleans', () => {
+      
+    });
+
+  });
+});
