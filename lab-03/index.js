6c8197b1fb63897fb6c9145eafea5d356fa27e79
Phil Fitch <mostreluctantly@gmail.com>
2019-09-23 18:40:35 -0700

    index.js tests

--- lab-03/index.js
+++ lab-03/index.js
@@ -1,5 +1,20 @@
 const DocumentCollection = require('./lib/document-collection');
 
-const documents = new DocumentCollection(/* path to use */);
+const toys = new DocumentCollection('./toys');
 
 // write some code to exercise your document collection
+
+const obj = {
+  name: 'rubic\'s cube',
+  shape: 'cube'
+};
+
+toys.save(obj);
+
+// toys.get('')
+//   .then(stuff => {
+//     console.log('get', stuff);
+//   });
+
+// toys.getAll()
+//   .then(all => console.log(all));
